9a4d020debce36c40cf434257b0be9247cb4e29a
devshutdown <cis290@pc22759.rvc.cc.il.us>
2016-09-19 17:34:24 -0500

    Week 5 initial changes
    Week 5

--- HideKeyBoard/ViewController.swift
+++ HideKeyBoard/ViewController.swift
@@ -17,67 +17,81 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtLast: UITextField!
     @IBOutlet weak var txtEmail: UITextField!
     
-// 1) Add MessageBox function using AlertView
+    // 1) Add MessageBox function using AlertView
     func MsgBox(message:String)
     {
         //Add Below Code from "Code for Steps.txt". Read Comments
         
         //Create Alert
-        
+        let alert = UIAlertView()
+        alert.title = "Alert"
+        alert.message = message
+        alert.addButtonWithTitle("OK")
+        alert.show()
     }
-// 2) Add touchesBegan function to catch screen tap and resign keyboard
+    
+    // 2) Add touchesBegan function to catch screen tap and resign keyboard
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         //Add Below Code from "Code for Steps.txt". Read Comments
         
         //forces resign first responder and hides keyboard
-       
-
+        txtFirst.endEditing(true)
+        txtLast.endEditing(true)
+        txtEmail.endEditing(true)
+        
         
     }
     
-// 3) Add DismissKeyboard function to resign keyboard on all textboxes
+    // 3) Add DismissKeyboard function to resign keyboard on all textboxes
     func DismissKeyboard(){
         //Add Below Code from "Code for Steps.txt". Read Comments
         
         //forces resign first responder and hides keyboard
-      
-
-       
+        txtFirst.endEditing(true)
+        txtLast.endEditing(true)
+        txtEmail.endEditing(true)
+        
+        
     }
     
-//  4) Add textFieldShouldReturn function. This is called when 'return' key pressed on any UITextField. return NO to ignore.
+    //  4) Add textFieldShouldReturn function. This is called when 'return' key pressed on any UITextField. return NO to ignore.
     func textFieldShouldReturn(textField: UITextField!) -> Bool     {
-         //Add Below Code from "Code for Steps.txt". Read Comments
-        
+        //Add Below Code from "Code for Steps.txt". Read Comments
+        textField.resignFirstResponder()
         return true;
     }
     
-//  5) Add textFieldDidBeginEditing function. ScrollPoint when entering UItextfied
+    //  5) Add textFieldDidBeginEditing function. ScrollPoint when entering UItextfied
     func textFieldDidBeginEditing(textField:UITextField){
-         //Add Below Code from "Code for Steps.txt". Read Comments
-       
-       
+        //Add Below Code from "Code for Steps.txt". Read Comments
+        var scrollPoint:CGPoint
+        scrollPoint = CGPointMake(0, textField.frame.origin.y)
+        ScrollView.setContentOffset(scrollPoint, animated: true)
+        
     }
     
-// 6) Add textFieldDidEndEditing function. ScrollPoint when done editing UItextfied
+    // 6) Add textFieldDidEndEditing function. ScrollPoint when done editing UItextfied
     func textFieldDidEndEditing(textField:UITextField){
-         //Add Below Code from "Code for Steps.txt". Read Comments
-            }
+        //Add Below Code from "Code for Steps.txt". Read Comments
+        ScrollView.setContentOffset(CGPointZero, animated: true)
+    }
     
-// 7) Add textViewDidBeginEditing function. ScrollPoint when entering UItextView
+    // 7) Add textViewDidBeginEditing function. ScrollPoint when entering UItextView
     func textViewDidBeginEditing(textField:UITextView){
-         //Add Below Code from "Code for Steps.txt". Read Comments
-        
+        //Add Below Code from "Code for Steps.txt". Read Comments
+        var scrollPoint:CGPoint
+        scrollPoint = CGPointMake(0, textField.frame.origin.y)
+        ScrollView.setContentOffset(scrollPoint, animated: true)
     }
     
-// 8) Add textViewDidEndEditing function. ScrollPoint when done ending UItextView
+    // 8) Add textViewDidEndEditing function. ScrollPoint when done ending UItextView
     func textViewDidEndEditing(textField:UITextView){
-         //Add Below Code from "Code for Steps.txt". Read Comments
-        
+        //Add Below Code from "Code for Steps.txt". Read Comments
+        ScrollView.setContentOffset(CGPointZero, animated: true)
     }
     
     
-// 9) Modify existing viewDidLoad function. Default Function to load when view is shown
+    // 9) Modify existing viewDidLoad function. Default Function to load when view is shown
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -85,38 +99,39 @@ class ViewController: UIViewController {
         //Add Below Code. Read Comments
         
         //Looks for single or multiple taps
-        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
         //Adds tap gesture to ScrollView which will call DismissKeyboard and hide keyboard
-       
+        ScrollView.addGestureRecognizer(tap)
         //gets ScreenSize of current device
-        
+        let size:CGRect = UIScreen.mainScreen().bounds
         //Go to left  = 0 pixels, top = 50 pixels, width or device in pixels, height of device in pixels
-       
+        ScrollView.frame = CGRectMake(0, 50, size.width, size.height)
         //Set focus on txtFirst
-      
+        txtFirst.becomeFirstResponder()
         
     }
     
-// 10) Default didReceiveMemoryWarning Function to dispose of memory automatically.Leave this function alone
+    // 10) Default didReceiveMemoryWarning Function to dispose of memory automatically.Leave this function alone
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-// 11) Modify btnBack function. Goes back to original ScrollView position
-    @IBAction func btnBack(sender: UIButton) {
-         //Add Below Code from "Code for Steps.txt". Fulfill Each Comment With Code
-        
-        //Set ScrollPoint and Go to ZERO location (Top) of ScrollView - animated
-      
-        
-        //set txtFirst as firstresponder
-       
-
-       
+        super.didReceiveMemoryWarning()
     }
     
-// 12) Modify btnView functions. Goes to position in ScrollView where the btnBack button is
+    // 11) Modify btnBack function. Goes back to original ScrollView position
+    @IBAction func btnBack(sender: UIButton) {
+        //Add Below Code from "Code for Steps.txt". Fulfill Each Comment With Code
+        
+        //Set ScrollPoint and Go to ZERO location (Top) of ScrollView - animated
+        ScrollView.setContentOffset(CGPointZero, animated: true)
+        
+        //set txtFirst as firstresponder
+        txtFirst.becomeFirstResponder()
+        
+        
+    }
+    
+    // 12) Modify btnView functions. Goes to position in ScrollView where the btnBack button is
     @IBAction func btnView(sender: UIButton) {
         //Add Below Code from "Code for Steps.txt". Fulfill Each Comment With Code
         
@@ -135,19 +150,39 @@ class ViewController: UIViewController {
         
     }
     
-//  13) Modify btnSave function. Validates UITextfields have content, formats text to place in UITextView txtContacts, Goes to location of btnBack button.
+    //  13) Modify btnSave function. Validates UITextfields have content, formats text to place in UITextView txtContacts, Goes to location of btnBack button.
     @IBAction func btnSave(sender: UIButton) {
         //Add Below Code from "Code for Steps.txt". Fulfill Each Comment With Code
-       
+        
         //hide keyboard
-       
+        DismissKeyboard()
         
         //validation that all fields are entered
-        
-        
-     }
+        if (txtFirst.text=="" || txtLast.text=="" || txtEmail.text=="")
+        {
+            MsgBox("All fields required, please correct")
+            
+        }
+            
+        else
+        {
+                if (txtContacts.text=="")
+                {
+                    txtContacts.text = "MyContacts \n"
+                }
+            
+            txtContacts.text = "\(txtContacts.text!) \n\(txtFirst.text!) \n\(txtEmail.text!) \n"
+            txtFirst.text = ""
+            txtLast.text = ""
+            txtEmail.text = ""
+            
+            var scrollPoint:CGPoint
+            
+            scrollPoint = CGPointMake(0, btnBack.frame.origin.y)
+            ScrollView.setContentOffset(scrollPoint, animated: true)
+        }
+    }
     
-
+    
     
 }
-
